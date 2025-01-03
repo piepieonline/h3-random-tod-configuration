49725bb0e813579167c192ec1bbf8cff68e86580
Kevin-Rudd <94417486+Kevin-Rudd@users.noreply.github.com>
2025-01-03 13:37:02 +1100

    - Adds Most Bricks

--- missions.js
+++ missions.js
@@ -1,51 +1,106 @@
 export const missions = {
+     "assembly:/_pro/scenes/missions/paris/_scene_fashionshowhit_01.entity": [
+        "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
+        "assembly:/_pro/scenes/missions/paris/tod_midday.brick"
+      ],
+    "assembly:/_PRO/Scenes/Missions/CoastalTown/_scene_octopus.entity": [
+        "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
+        "assembly:/_pro/scenes/missions/coastaltown/tod_wot_sunset.brick",
+        "assembly:/_pro/scenes/missions/coastaltown/tod_ebola_night.brick"
+      ],
+   "assembly:/_PRO/Scenes/Missions/Marrakesh/_scene_spider.entity": [
+        "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
+        "assembly:/_pro/scenes/missions/marrakesh/gilded_tod_summer_night_clear.brick"
+      ],
+   "assembly:/_PRO/Scenes/Missions/Bangkok/_scene_tiger.entity": [
+        "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
+        "assembly:/_pro/scenes/missions/bangkok/tod_midday.brick",
+       "assembly:/_pro/scenes/missions/bangkok/tod_zika_night.brick"
+      ],
+      "assembly:/_PRO/Scenes/Missions/Colorado_2/_scene_bull.entity": [
+        "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
+        "assembly:/_pro/scenes/missions/colorado_2/lighting_night.brick",
+        "assembly:/_pro/scenes/missions/colorado_2/lighting_day.brick"
+      ],
+   "assembly:/_PRO/Scenes/Missions/Hokkaido/_Scene_Mission_SnowCrane.entity": [
+        "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
+        "assembly:/_pro/scenes/missions/hokkaido/tod_flu.brick",
+       "assembly:/_pro/scenes/missions/hokkaido/tod_bluenight.brick
+      ],
     "assembly:/_pro/scenes/missions/sheep/scene_sheep.entity": [
         "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
-        "assembly:/_pro/scenes/missions/sheep/tod_twilight.brick"
-      ],
-      "assembly:/_pro/scenes/missions/golden/mission_gecko/scene_gecko_basic.entity": [
-        "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
-        "assembly:/_pro/scenes/missions/golden/mission_gecko/midday_gecko.brick"
-      ],
-      "assembly:/_pro/scenes/missions/edgy/mission_fox/scene_fox_basic.entity": [
-        "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
-        "assembly:/_pro/scenes/missions/edgy/mission_fox/variant_easter.brick"
+        "assembly:/_pro/scenes/missions/sheep/tod_twilight.brick",
+        "assembly:/_pro/scenes/missions/sheep/tod_sunset.brick",
+        "assembly:/_pro/scenes/missions/sheep/tod_midday.brick"
       ],
       "assembly:/_pro/scenes/missions/miami/scene_flamingo.entity": [
         "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
-        "assembly:/_pro/scenes/missions/miami/tod_sunset.brick"
+        "assembly:/_pro/scenes/missions/miami/tod_sunset.brick",
+        "assembly:/_pro/scenes/missions/miami/tod_night.brick"
       ],
-      "assembly:/_pro/scenes/missions/paris/_scene_fashionshowhit_01.entity": [
+   "assembly:/_PRO/Scenes/Missions/Colombia/scene_hippo.entity": [
         "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
-        "assembly:/_pro/scenes/missions/paris/tod_midday.brick"
+        "assembly:/_pro/scenes/missions/colombia/tod_night.brick"
+      ],
+      "assembly:/_pro/scenes/missions/mumbai/scene_mongoose.entity": [
+        "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
+        "assembly:/_pro/scenes/missions/mumbai/tod_night.brick",
+        "assembly:/_pro/scenes/missions/mumbai/tod_midday.brick"
+      ],
+      "assembly:/_pro/scenes/missions/skunk/scene_skunk.entity": [
+        "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
+        "assembly:/_pro/scenes/missions/skunk/tod_sunset.brick",
+        "assembly:/_pro/scenes/missions/skunk/tod_night.brick"
+      ],
+      "assembly:/_pro/scenes/missions/theark/scene_magpie.entity": [
+        "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
+        "assembly:/_pro/scenes/missions/theark/tod_midday.brick",
+        "assembly:/_pro/scenes/missions/theark/tod_sunset.brick"
+      ],
+      "assembly:/_pro/scenes/missions/salty/mission_seagull/scene_seagull.entity": [
+        "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
+        ""
       ],
       "assembly:/_pro/scenes/missions/greedy/mission_raccoon/scene_raccoon_basic.entity": [
         "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
         "assembly:/_pro/scenes/missions/greedy/mission_raccoon/raccoon_midday.brick"
       ],
-      "assembly:/_pro/scenes/missions/skunk/scene_skunk.entity": [
+   "assembly:/_pro/scenes/missions/opulent/mission_stingray/scene_stingray_basic.entity": [
         "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
-        "assembly:/_pro/scenes/missions/skunk/tod_sunset.brick"
+        "assembly:/_pro/scenes/missions/opulent/mission_stingray/tod_night.brick"
       ],
-      "assembly:/_pro/scenes/missions/theark/scene_magpie.entity": [
+      "assembly:/_pro/scenes/missions/golden/mission_gecko/scene_gecko_basic.entity": [
         "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
-        "assembly:/_pro/scenes/missions/theark/tod_midday.brick"
+        "assembly:/_pro/scenes/missions/golden/mission_gecko/midday_gecko.brick"
       ],
-      "assembly:/_pro/scenes/missions/mumbai/scene_mongoose.entity": [
+      "assembly:/_pro/scenes/missions/ancestral/scene_bulldog.entity": [
         "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
-        "assembly:/_pro/scenes/missions/mumbai/tod_night.brick"
+        "assembly:/_pro/scenes/missions/ancestral/tod_night.brick",
+          "assembly:/_pro/scenes/missions/ancestral/tod_midday.brick"
+      ],
+      "assembly:/_pro/scenes/missions/edgy/mission_fox/scene_fox_basic.entity": [
+        "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
+        "assembly:/_pro/scenes/missions/edgy/mission_fox/variant_easter.brick",
+          "assembly:/_pro/scenes/missions/edgy/mission_fox/tod_midday.brick",
+          "assembly:/_pro/scenes/missions/edgy/mission_fox/tod_sunset.brick"
+      ],
+   "assembly:/_PRO/Scenes/missions/Wet/scene_rat_basic.entity": [
+        "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
+        "assembly:/_pro/scenes/missions/wet/tod_midday.brick",
+       "assembly:/_pro/scenes/missions/wet/tod_cloudy.brick"
       ],
       "assembly:/_pro/scenes/missions/elegant/scene_llama.entity": [
         "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
         "assembly:/_pro/scenes/missions/elegant/tod_midday.brick"
       ],
-      "assembly:/_pro/scenes/missions/colorado_2/_scene_mission_bull.entity": [
+   "assembly:/_PRO/Scenes/Missions/trapped/scene_wolverine.entity": [
         "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
-        "assembly:/_pro/scenes/missions/colorado_2/lighting_night.brick"
+        ""
       ],
-      "assembly:/_pro/scenes/missions/ancestral/scene_bulldog.entity": [
+   "assembly:/_PRO/Scenes/Missions/Rocky/scene_dugong.entity": [
         "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
-        "assembly:/_pro/scenes/missions/ancestral/tod_night.brick"
+        "assembly:/_PRO/Scenes/Missions/Rocky/tod_midday.brick",
+       "assembly:/_PRO/Scenes/Missions/Rocky/tod_sunset.brick"
       ]
 }
 
