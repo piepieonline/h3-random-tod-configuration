4005c4f99720e15dd8ab081afd9122ca71d6f4bb
Kevin-Rudd <94417486+Kevin-Rudd@users.noreply.github.com>
2025-01-07 21:16:26 +1100

    Completes Missions.js
    Capitalisations to be tested. Final check of each brick TBD.

--- missions.js
+++ missions.js
@@ -1,16 +1,27 @@
 export const missions = {
   "assembly:/_pro/scenes/missions/paris/_scene_fashionshowhit_01.entity": [
     "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
-    "assembly:/_pro/scenes/missions/paris/tod_midday.brick"
+    "assembly:/_pro/scenes/missions/paris/tod_midday.brick",
+    "assembly:/_pro/scenes/missions/paris/tod_night.brick",
+    "assembly:/_PRO/Scenes/Missions/Paris/Location_parisnoel.brick"
   ],
   "assembly:/_pro/scenes/missions/coastaltown/_scene_octopus.entity": [
     "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
     "assembly:/_pro/scenes/missions/coastaltown/tod_wot_sunset.brick",
     "assembly:/_pro/scenes/missions/coastaltown/tod_ebola_night.brick"
   ],
+  "assembly:/_pro/scenes/missions/coastaltown/_scene_mission_mamba.entity": [
+    "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
+    "assembly:/_pro/scenes/missions/coastaltown/tod_day_landslide.brick"
+  ],
   "assembly:/_pro/scenes/missions/marrakesh/_scene_spider.entity": [
     "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
-    "assembly:/_pro/scenes/missions/marrakesh/gilded_tod_summer_night_clear.brick"
+    "assembly:/_pro/scenes/missions/marrakesh/gilded_tod_summer_night_clear.brick",
+    "assembly:/_pro/scenes/missions/marrakesh/lighting_dusk.brick"
+  ],
+  "assembly:/_pro/scenes/missions/marrakesh/_scene_python.entity": [
+    "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
+    "assembly:/_pro/scenes/missions/marrakesh/tod_ahbos_day.brick"
   ],
   "assembly:/_pro/scenes/missions/bangkok/_scene_tiger.entity": [
     "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
@@ -59,7 +70,7 @@ export const missions = {
   ],
   "assembly:/_pro/scenes/missions/salty/mission_seagull/scene_seagull.entity": [
     "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
-    ""
+    "assembly:/_pro/scenes/missions/salty/mission_seagull/tod_rainy_night.brick"
   ],
   "assembly:/_pro/scenes/missions/greedy/mission_raccoon/scene_raccoon_basic.entity": [
     "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
@@ -95,7 +106,7 @@ export const missions = {
   ],
   "assembly:/_pro/scenes/missions/trapped/scene_wolverine.entity": [
     "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
-    ""
+    "assembly:/_pro/scenes/missions/trapped/tod_day.brick"
   ],
   "assembly:/_pro/scenes/missions/rocky/scene_dugong.entity": [
     "assembly:/_pro/scenes/missions/thefacility/outfits_prologue.brick",
@@ -106,7 +117,83 @@ export const missions = {
 
 export const missionAliases = {
   "assembly:/_pro/scenes/missions/paris/_scene_fashionshowhit_01.entity": [
-    "assembly:/_pro/scenes/missions/paris/_scene_paris.entity"
+    "assembly:/_pro/scenes/missions/paris/_scene_paris.entity",
+    "assembly:/_pro/scenes/missions/paris/_scene_paris_torenia.entity"
+  ],
+   "assembly:/_pro/scenes/missions/coastaltown/_scene_octopus.entity": [
+    "assembly:/_PRO/Scenes/Missions/CoastalTown/Mission01.entity"
+  ],
+   "assembly:/_pro/scenes/missions/coastaltown/_scene_mission_mamba.entity": [
+    "assembly:/_PRO/Scenes/Missions/CoastalTown/_scene_mamba.entity"
+  ],
+   "assembly:/_pro/scenes/missions/marrakesh/_scene_spider.entity": [
+    "assembly:/_PRO/Scenes/Missions/Marrakesh/_Scene_Mission_Spider.entity"
+  ],
+   "assembly:/_pro/scenes/missions/marrakesh/_scene_python.entity": [
+    "assembly:/_PRO/Scenes/Missions/Marrakesh/_Scene_Mission_Python.entity",
+    "assembly:/_pro/scenes/missions/marrakesh/_scene_python_hellebore.entity"
+  ],
+   "assembly:/_pro/scenes/missions/bangkok/_scene_tiger.entity": [
+    "assembly:/_PRO/Scenes/Missions/Bangkok/_Scene_Mission_Tiger.entity"
+  ],
+   "assembly:/_pro/scenes/missions/colorado_2/_scene_mission_bull.entity": [
+    "assembly:/_PRO/Scenes/Missions/Colorado_2/_scene_bull.entity"
+  ],
+   "assembly:/_pro/scenes/missions/hokkaido/_scene_mission_snowcrane.entity": [
+    "assembly:/_PRO/Scenes/Missions/Hokkaido/_Scene_SnowCrane.entity",
+    "assembly:/_pro/scenes/missions/hokkaido/scene_mamushi.entity",
+    "assembly:/_pro/scenes/missions/Hokkaido/_scene_snowcrane_tumbleweed.entity"
+  ],
+   "assembly:/_pro/scenes/missions/sheep/scene_sheep.entity": [
+    "assembly:/_pro/scenes/missions/sheep/scene_opuntia.entity",
+    "assembly:/_pro/scenes/missions/sheep/scene_adonis.entity
+  ],
+   "assembly:/_pro/scenes/missions/miami/scene_flamingo.entity": [
+    "assembly:/_pro/scenes/missions/miami/scene_cottonmouth.entity",
+    "assembly:/_pro/scenes/missions/miami/scene_et_sambuca.entity"
+  ],
+   "assembly:/_pro/scenes/missions/colombia/scene_hippo.entity": [
+    "assembly:/_pro/scenes/missions/colombia/scene_anaconda.entity",
+     "assembly:/_pro/scenes/missions/colombia/scene_hippo_calluna.entity",
+     "assembly:/_pro/scenes/missions/colombia/scene_hippo_rafflesia.entity"
+  ],
+   "assembly:/_pro/scenes/missions/mumbai/scene_mongoose.entity": [
+    "assembly:/_pro/scenes/missions/mumbai/scene_kingcobra.entity",
+     "assembly:/_pro/scenes/missions/mumbai/_scene_mongoose_ashoka.entity"
+  ],
+   "assembly:/_pro/scenes/missions/skunk/scene_skunk.entity": [
+    "assembly:/_pro/scenes/missions/skunk/scene_gartersnake.entity"
+  ],
+   "assembly:/_pro/scenes/missions/theark/scene_magpie.entity": [
+    "assembly:/_pro/scenes/missions/theark/_scene_magpie_pansy.entity",
+     "assembly:/_pro/scenes/missions/theark/_scene_magpie_lotus.entity",
+     "assembly:/_pro/scenes/missions/theark/scene_penicillin.entity"
+  ],
+   "assembly:/_pro/scenes/missions/greedy/mission_raccoon/scene_raccoon_basic.entity": [
+    "assembly:/_pro/scenes/missions/Greedy/mission_raccoon/scene_raccoon_basic_Dandelion.entity"
+  ],
+   "assembly:/_pro/scenes/missions/opulent/mission_stingray/scene_stingray_basic.entity": [
+    "assembly:/_pro/scenes/missions/opulent/mission_stingray/scene_stingray_arcticthyme.entity"
+  ],
+   "assembly:/_pro/scenes/missions/golden/mission_gecko/scene_gecko_basic.entity": [
+    "assembly:/_PRO/Scenes/Missions/golden/mission_gecko/scene_gecko_sheepsorrel.entity",
+    "assembly:/_PRO/Scenes/Missions/golden/mission_gecko/scene_gibson.entity",
+    "assembly:/_PRO/Scenes/Missions/golden/mission_gecko/scene_gecko_angelica.entity",
+    "assembly:/_PRO/Scenes/Missions/golden/mission_gecko/scene_gecko_lunaria.entity",
+    "assembly:/_PRO/Scenes/Missions/golden/mission_gecko/scene_gecko_desertrose.entity"
+  ],
+   "assembly:/_pro/scenes/missions/ancestral/scene_bulldog.entity": [
+    "assembly:/_PRO/Scenes/Missions/Ancestral/scene_bulldog_fern.entity",
+    "assembly:/_PRO/scenes/missions/ancestral/scene_ancestral_vesper.entity"
+  ],
+   "assembly:/_pro/scenes/missions/edgy/mission_fox/scene_fox_basic.entity": [
+    "assembly:/_PRO/Scenes/Missions/Edgy/mission_fox/scene_fox_contractcreation.entity",
+    "assembly:/_PRO/Scenes/Missions/Edgy/mission_fox/scene_fox_NightPhlox.entity",
+    "assembly:/_PRO/Scenes/Missions/Edgy/mission_fox/scene_fox_smilax.entity",
+    "assembly:/_PRO/Scenes/Missions/Edgy/mission_fox/scene_fox_cornflower.entity",
+    "assembly:/_PRO/Scenes/Missions/Edgy/mission_fox/scene_ambrosia.entity",
+    "assembly:/_PRO/Scenes/Missions/Edgy/mission_fox/scene_radler.entity",
+    "assembly:/_PRO/Scenes/Missions/Edgy/mission_fox/scene_fox_tomorrowland.entity"
   ],
   "assembly:/_pro/scenes/missions/wet/scene_rat_basic.entity": [
     "assembly:/_pro/scenes/missions/wet/scene_wet_makoyana.entity",
@@ -116,6 +203,16 @@ export const missionAliases = {
     "assembly:/_pro/scenes/missions/wet/scene_rat_elusive_redsnapper.entity",
     "assembly:/_pro/scenes/missions/wet/scene_wet_lambic.entity"
   ],
+   "assembly:/_pro/scenes/missions/elegant/scene_llama.entity": [
+    "assembly:/_pro/scenes/missions/elegant/scene_whitedryas.entity",
+    "assembly:/_pro/scenes/missions/elegant/scene_llama_jacaranda.entity",
+    "assembly:/_pro/scenes/missions/elegant/scene_frangipani.entity",
+    "assembly:/_pro/scenes/missions/elegant/scene_llama_elusive_jockeyclub.entity",
+    "assembly:/_pro/scenes/missions/elegant/scene_llama_elusive_clerico.entity"
+  ],
+   "assembly:/_pro/scenes/missions/trapped/scene_wolverine.entity": [
+    "assembly:/_pro/scenes/missions/trapped/scene_bellflower.entity"
+  ]
 };
 
 export const offbydefault = [
